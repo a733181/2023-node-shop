e173b070d85c51cbb7becc6e6afb10e78cdf2722
Syuan <a733181@gmail.com>
2023-01-16 15:29:40 +0800

    init

---
+++ middleware/content.js
@@ -0,0 +1,7 @@
+export default (type) => (req, res, next) => {
+  if (!req.headers['content-type'] || !req.headers['content-type'].includes(type)) {
+    res.status(400).json({ success: false, message: '格式錯誤' });
+    return;
+  }
+  next();
+};
